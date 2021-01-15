ea65984ab7a9f0bcbd886092460f6899e9f158cf
Prabin Gurung <prabingurung@Pramilas-MacBook-Pro.local>
2021-01-15 17:55:24 +0000

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import React from 'react';
+import logo from './mock-up.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <iframe width="375" height="812" src="https://xd.adobe.com/embed/a84b9d61-1718-45d9-afff-97952a46cc62-fd18/" frameborder="0" className="overlapp-bottom"></iframe>
     </div>
   );
 }
